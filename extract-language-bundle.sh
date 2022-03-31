ad13bcc4a583bdb47ecbce4079a19e3a91b3b0e5
Andy Miller <andymiller@fullstory.com>
2022-03-31 12:44:52 -0400

    Test commit to kick off smartling

--- extract-language-bundle.sh
+++ extract-language-bundle.sh
@@ -6,5 +6,7 @@ npx formatjs extract "src/**/*.{ts,tsx}" --out-file "src/lang/defaultMessages.js
 # compile messages into english bundle
 npx formatjs compile "src/lang/defaultMessages.json" --out-file "src/lang/messages_en.json"
 
+cp "src/lang/messages_en.json" "src/lang/messages.json"
+
 # generate fake french bundle for testing
 jq '. |= map_values(.+ " 🇫🇷")' "src/lang/messages_en.json" > src/lang/messages_fr.json
